ab6fa66c7325167eeb5c5820a33c928e78933d9c
jessicaseto <jes.seto@gmail.com>
2018-12-03 18:27:54 -0800

    Initial commit with starter google map insert using google-maps-react

--- src/App.js
+++ src/App.js
@@ -1,28 +1,15 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import {Map, GoogleApiWrapper} from 'google-maps-react';
 
-class App extends Component {
+export class MapContainer extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <Map google={this.props.google} zoom={14}>
+      </Map>
     );
   }
 }
 
-export default App;
+export default GoogleApiWrapper({
+  apiKey: 'AIzaSyBqpBnwVoGdiPfRLdW08dfLmbluuL_f1Po'
+})(MapContainer)
