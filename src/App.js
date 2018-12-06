8bbb35f403235993b461eb2d03136f745d949d51
jessicaseto <jes.seto@gmail.com>
2018-12-05 17:50:14 -0800

    Adding visibility toggle for sidebar on hamburger menu click

--- src/App.js
+++ src/App.js
@@ -1,21 +1,39 @@
 import React, { Component } from 'react';
 import Sidebar from './Sidebar';
 import MapContainer from './MapContainer';
+import MediaQuery from 'react-responsive';
 import './App.css';
 
 export class App extends Component {
+  // State
+  state = {
+    sidebarVisible: false
+  };
+
   // Styles
   containerStyles = {
     width: '100%',
     height: '100%'
   };
 
+  toggleNav = () => {
+    this.setState({
+      sidebarVisible: !this.state.sidebarVisible
+    });
+  };
+
   render() {
     return (
-      <div
-        style={this.containerStyles}
-      >
-        <Sidebar/>
+      <div style={this.containerStyles}>
+        <MediaQuery query="(max-width: 600px)">
+          <button
+            className="hamburger"
+            onClick={this.toggleNav}
+          >&#9776;</button>
+          {!this.state.sidebarVisible && (
+            <Sidebar/>
+          )}
+        </MediaQuery>
         <MapContainer/>
       </div>
     );
