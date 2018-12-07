658d68fa6e75766992457682478e28f59ff662c7
jessicaseto <jes.seto@gmail.com>
2018-12-07 15:28:03 -0800

    Added DebounceInput component for filtering list of hikes

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -1,9 +1,21 @@
 import React, { Component } from 'react';
+import DebounceInput from 'react-debounce-input';
 
 class Sidebar extends Component {
+  // State
+  state = {
+    query: ''
+  };
+
   handleHikeClick = (marker) => {
     // Activate marker
     this.props.activateMarker(marker);
+  };
+
+  handleInputChange = (event) => {
+    this.setState({
+      query: event.target.value
+    }, () => {console.log(this.state.query)});
   };
 
   render() {
@@ -12,6 +24,12 @@ class Sidebar extends Component {
         className={"sidebar " + this.props.sidebarVisible}
       >
         <h2>List of Hikes</h2>
+        <DebounceInput
+          className="hikes-search"
+          debounceTimeout={300}
+          type="text"
+          onChange={this.handleInputChange}
+        />
         <ul className="hikes-list">
           {(this.props.markers.length > 0) && (
             this.props.markers.map((marker) =>
